b2c85c33cc5b02bb1631f6c5b20057e792cdf502
川端進悟 <shingo@KsPC.local>
2017-01-29 08:22:03 +0900

    詳細画面

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def markdown2html(text)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+      autolink: true,
+      space_after_headers: true,
+      no_intra_emphasis: true,
+      fenced_code_blocks: true,
+      tables: true,
+      hard_wrap: true,
+      xhtml: true,
+      lax_html_blocks: true,
+      strikethrough: true)
+    markdown.render(text).html_safe
+  end
 end
