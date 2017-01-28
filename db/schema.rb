1aa042b2ece833321dc3263bbe41b49b6708a5b4
川端進悟 <shingo@KsPC.local>
2017-01-29 05:34:02 +0900

    記事にカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170128202555) do
+ActiveRecord::Schema.define(version: 20170128203217) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",      null: false
-    t.string   "body",       null: false
+    t.text     "body",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
